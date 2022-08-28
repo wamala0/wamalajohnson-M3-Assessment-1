6454085df0d73daf77bef00b55f1889f2db466ec
wamala0 <112074839+wamala0@users.noreply.github.com>
2022-08-28 13:04:21 +0300

    Add files via upload

---
+++ screens/Splash.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet,} from 'react-native'
+import maid from '../src/maid.jpg';
+
+const Splash = ({navigation}) => {
+
+    setTimeout(()=>{
+        navigation.navigate('Login')
+        },2000 )
+    return(
+      <View style={styles.container}>
+        <div>
+        <img src={maid} alt=""/>
+        </div>
+        <Text style={{fontSize: 20, color:"#fff"}}>
+          Just a SplashScreen
+        </Text>
+        
+
+      </View>
+    )
+  }
+ 
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   backgroundColor: '#008080',
+   
+  }
+});
+export default Splash;
