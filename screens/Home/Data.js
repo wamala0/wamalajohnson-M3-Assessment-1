043526f8a735be3aee098f6dca7e621b06cb97fe
wamala0 <112074839+wamala0@users.noreply.github.com>
2022-08-29 10:53:37 +0300

    Add files via upload

---
+++ screens/Home/Data.js
@@ -0,0 +1,25 @@
+
+const serviceData = [
+    {  
+        name:"Wiring Services",
+        category:"Electrician",
+        numberCompany:"55",
+        bgColor: "#c51162",
+      
+    },
+    {
+        name:"Plumber Services",
+        category:"Plumber",
+        numberCompany:"35",
+        bgColor: "#c51162",
+       
+    },
+    {
+        name:"Hair Dresser",
+        category:"Salon",
+        numberCompany:"25",
+        bgColor: "#c51162",
+       
+    }
+]
+export default serviceData
--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,1 +1,83 @@
+import  React from 'react'
+import { StyleSheet, Text, View,TouchableOpacity,Dimensions,image} from 'react-native';
+import { FlatList, SafeAreaView } from 'react-native-web';
+import serviceData from './Data';
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+
+const Home= ({navigation}) => {
+  return (
+    <SafeAreaView>
+<FlatList
+data={serviceData}
+contentInset={{padding:12}}
+renderItem={({item}) =>{
+  return(
+    <TouchableOpacity style={{margin:10, height:windowHeight*0.2,backgroundColor:item.bgColor,borderRadius:10}}>
+      <View style={{backgroundColor:item.bgColor}}>
+      <Text style={styles.category}>
+        {item.category}
+      </Text>
+      <Text style={styles.name}>
+        {item.name}
+      </Text>
+      <Text>
+        {item.numberCompany}
+      </Text>
+      </View>
+      
+    </TouchableOpacity>
+  )
+}}
+>
+</FlatList>
+<View style={styles.btnContainer}>
+     <TouchableOpacity style={styles.userBtn} onPress ={() => navigation.navigate('Details')}>
+        <Text style={styles.btnTxt}>
+            See Details Page
+        </Text>
+     </TouchableOpacity >
+     </View>
+ </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'skyblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btnContainer: {
+    justifyContent: 'center',
+    
+    },
+    userBtn: {
+     backgroundColor:"green",
+     underlineColorAndroid:'rgba(0,0,0,0)',
+      padding:10,
+      width:"30%",
+      margin:15,
+     borderRadius:10
+      
+     },
+     btnTxt: {
+         fontSize: 15,
+         textAlign:"center",
+         fontWeight: "bold"
+         },
+    category:{
+       fontSize:24,
+       fontWeight:200,
+       color:'white'
+      },
+      name:{
+       fontSize:18,
+       color:'white'
+      },
+});
+
+export default Home;
